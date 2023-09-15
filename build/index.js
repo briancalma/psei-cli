08953726b7f92c122fc091582b6d8d1de0549a70
Brian Calma <briancalmadevacc@gmail.com>
2023-09-15 23:21:05 +0800

    initial fetch implementation

--- build/index.js
+++ build/index.js
@@ -1,4 +1,34 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
 const figlet = require("figlet");
+const { Command } = require("commander");
 const appName = "PSEI-cli";
+const program = new Command();
 console.log(figlet.textSync(appName));
+program
+    .version("1.0.0")
+    .description("A simple cli tool for fetching Philippine Stocks details.")
+    .option("-s, --stock <stock>", "fetched latest stock data using supplied stock symbol")
+    .parse(process.argv);
+const options = program.opts();
+// console.log("[options]:", options);
+const fetchStock = (stock) => __awaiter(void 0, void 0, void 0, function* () {
+    const response = yield axios_1.default.get(`http://phisix-api.appspot.com/stocks/${stock}.json`);
+    console.log("[fetchStock]:", response.data);
+});
+if (options.stock) {
+    fetchStock(options.stock);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,35 @@
+import axios from "axios";
+
 const figlet = require("figlet");
+const { Command } = require("commander");
 
 const appName: string = "PSEI-cli";
 
+const program = new Command();
+
 console.log(figlet.textSync(appName));
+
+program
+  .version("1.0.0")
+  .description("A simple cli tool for fetching Philippine Stocks details.")
+  .option(
+    "-s, --stock <stock>",
+    "fetched latest stock data using supplied stock symbol"
+  )
+  .parse(process.argv);
+
+const options = program.opts();
+
+// console.log("[options]:", options);
+
+const fetchStock = async (stock: string) => {
+  const response = await axios.get(
+    `http://phisix-api.appspot.com/stocks/${stock}.json`
+  );
+
+  console.log("[fetchStock]:", response.data);
+};
+
+if (options.stock) {
+  fetchStock(options.stock);
+}
