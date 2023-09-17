748662009b9a01357b1dee19e8c120104f353ab7
Brian Calma <briancalmadevacc@gmail.com>
2023-09-17 12:18:26 +0800

    feat: added loading spinner

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,127 @@
+import { Command } from "commander";
+import ora from "ora";
 import axios from "axios";
+import chalk from "chalk";
+import Table from "cli-table3";
 
-const figlet = require("figlet");
-const { Command } = require("commander");
+export const PHISIX_API_URL = "http://phisix-api3.appspot.com/";
+export const APP_NAME = "psei-cli";
+export const APP_DESCRIPTION =
+  "A simple cli tool for fetching Philippine Stocks details.";
+export const APP_VERSION = "1.0.0";
 
-const appName: string = "PSEI-cli";
+export type Price = {
+  currency: string;
+  amount: number;
+};
+
+export type Stock = {
+  name: string;
+  price: Price;
+  percent_change: number;
+  volume: number;
+  symbol: string;
+};
+
+export type PhisixApiResponse = {
+  stock: Stock[];
+  as_of: string;
+};
 
 const program = new Command();
 
-console.log(figlet.textSync(appName));
+// console.log(figlet.textSync(APP_NAME));
 
 program
-  .version("1.0.0")
-  .description("A simple cli tool for fetching Philippine Stocks details.")
+  .version(APP_VERSION)
+  .description(APP_DESCRIPTION)
   .option(
-    "-s, --stock <stock>",
-    "fetched latest stock data using supplied stock symbol"
+    "-s, --stock <symbol>",
+    "fetched latest stock data using supplied stock symbol",
+    "BDO"
   )
   .parse(process.argv);
 
 const options = program.opts();
 
-// console.log("[options]:", options);
-
-const fetchStock = async (stock: string) => {
-  const response = await axios.get(
-    `http://phisix-api.appspot.com/stocks/${stock}.json`
-  );
-
-  console.log("[fetchStock]:", response.data);
+const moneyToPhpFormat = (amount: number): string => {
+  const formatter = new Intl.NumberFormat("en-PH", {
+    style: "currency",
+    currency: "PHP",
+  });
+  const formattedAmount = formatter.format(amount);
+  return formattedAmount;
 };
 
-if (options.stock) {
-  fetchStock(options.stock);
-}
+const formatDate = (dateString: string): string => {
+  // Create a new Date object from the date string
+  const date = new Date(dateString);
+
+  // Array of month names for formatting
+  const monthNames = [
+    "Jan.",
+    "Feb.",
+    "Mar.",
+    "Apr.",
+    "May",
+    "Jun.",
+    "Jul.",
+    "Aug.",
+    "Sept.",
+    "Oct.",
+    "Nov.",
+    "Dec.",
+  ];
+
+  // Extracting individual components
+  const year = date.getFullYear();
+  const month = monthNames[date.getMonth()];
+  const day = date.getDate();
+  const hour = date.getHours();
+  const minutes = date.getMinutes();
+
+  // Formatting AM or PM
+  let period = "AM";
+  if (hour >= 12) {
+    period = "PM";
+  }
+  const hour12 = hour > 12 ? hour - 12 : hour === 0 ? 12 : hour;
+
+  // Formatting minutes to have leading zeros
+  const formattedMinutes = minutes.toString().padStart(2, "0");
+
+  // Putting it all together
+  return `${month} ${day}, ${year} ${hour12}:${formattedMinutes} ${period}`;
+};
+
+const fetchStock = async (symbol: null | string) => {
+  const spinner = ora("Fetching stock data...");
+  const table = new Table();
+
+  try {
+    spinner.start();
+    const response = await axios.get(`${PHISIX_API_URL}/stocks/${symbol}.json`);
+
+    const stockData: PhisixApiResponse = response.data;
+    spinner.stop();
+    // console.log("[stockData]:", stockData);
+
+    const lastUpdatedAt = formatDate(stockData.as_of);
+
+    stockData.stock?.forEach((stock: Stock) => {
+      table.push({ Name: stock.name });
+      table.push({ Symbol: stock.symbol });
+      table.push({ Price: moneyToPhpFormat(stock.price.amount) });
+      table.push({ "Percent Change": stock.percent_change });
+      table.push({ Volume: stock.volume });
+      table.push({ "As of": lastUpdatedAt });
+    });
+
+    console.log(table.toString());
+  } catch (error: any) {
+    spinner.stop();
+    console.log(chalk.red("Failed to fetch stock data."));
+  }
+};
+
+if (options.stock) fetchStock(options.stock);
